eb2c8e4922c1ac9585bfb22c626e151f43fa7fe0
emanuelgamer011 <90418449+emanuelgamer011@users.noreply.github.com>
2022-05-29 13:24:47 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,21 +4,40 @@ TriggerEvent("esx:getSharedObject", function(obj)
     ESX = obj
 end)
 
+
+function canPlayerCarry(playerId, itemName, itemCount)
+
+    local canCarry = false
+    local xPlayer = ESX.GetPlayerFromId(playerId)
+
+    if (xPlayer.canCarryItem) then
+        canCarry = xPlayer.canCarryItem(itemName, itemCount)
+    else
+        local item = xPlayer.getInventoryItem(itemName)
+        canCarry = (item.limit == -1) or ((item.count + itemCount) <= item.limit)
+    end
+
+    return canCarry
+end
+
 RegisterNetEvent('esx_spendormachines:Cashier')
 AddEventHandler('esx_spendormachines:Cashier', function(price, item, amount)
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
 
-    print(item)
-    print(amount)
 
+    if canPlayerCarry(source, item, amount) then
         xPlayer.removeMoney(price)
     
         xPlayer.addInventoryItem(item, amount)
-    
+        TriggerClientEvent('okokNotify:Alert', source, "EXITO", 'Has comprado <span style="color:green">'.. ESX.GetItemLabel(item) ..'</span> x'.. amount ..' por <span style="color:green">$' .. price .. '</span> Gracias por su compra', 3000, 'success')
+
+    else
+        TriggerClientEvent('okokNotify:Alert', source, "", 'No tienes fuerza para llevar mas <span style="color:green">'.. ESX.GetItemLabel(item) ..'</span>', 3000, 'warning')
+
+    end
     -- pNotify('Ha comprado productos por <span style="color: green">$' .. price .. '</span>', 'success', 3000)
 
-    TriggerClientEvent('okokNotify:Alert', source, "EXITO", 'Has comprado '.. ESX.GetItemLabel(item) ..' x'.. amount ..' por <span style="color:green">$' .. price .. '</span> Gracias por su compra', 3000, 'success')
 
 
 end)
